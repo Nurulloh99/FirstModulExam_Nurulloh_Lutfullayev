385eec93943efdd4d86e12491c6bb0121edadc96
Nurulloh99 <nooroonus@gmail.com>
2024-11-30 10:19:23 +0500

    First modul exam has been completed successfully

--- FirstModulExam_NurullohLutfullayev/Models/Phone.cs
+++ FirstModulExam_NurullohLutfullayev/Models/Phone.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FirstModulExam_NurullohLutfullayev.Models
+namespace FirstModulExam_NurullohLutfullayev.Models;
+
+public class Phone
 {
-    internal class Phone
-    {
-    }
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Model { get; set; }
+    public Double Price { get; set; }
+    public string Color { get; set; }
 }
--- FirstModulExam_NurullohLutfullayev/Program.cs
+++ FirstModulExam_NurullohLutfullayev/Program.cs
@@ -3,8 +3,50 @@
     internal class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello, World!");
+            
+        }
+
+
+        public static int GetNumbersWithTwoDigits(List<int> numbers)
+        {
+            var amountOfNumbers = 0;
+
+            foreach(var number in numbers)
+            {
+                if(number > 9 && number < 100)
+                {
+                    amountOfNumbers++;
+                }
+            }
+            return amountOfNumbers;
+        }
+
+
+        public static string GetStringWithoutTwoLettersFromLast(string text)
+        {
+            text = text.Remove(text.Length - 2);
+            return text;
+        }
+
+
+        public static bool GetTextWithCapitalOfFirstLetter(List<string> text)
+        {
+            foreach(var word in text)
+            {
+                if (Char.IsLower(word[0]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        public static bool StartAndEndWithQovun(string text)
+        {
+            return text.StartsWith("qovun") && text.EndsWith("qovun");
         }
     }
 }
